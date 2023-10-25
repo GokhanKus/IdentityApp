38b7d6167ae8a99a8a78edd1ac23f6470f8ecb6f
GokhanKus <gkus1998@hotmail.com>
2023-10-25 10:07:09 +0300

    Identity.EfCore ve Sqlite paketler yüklendi, context sınıfı eklendi  kalıtım :IdentityDbContext

---
+++ Data/IdentityContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Data
+{
+    //normalde context.cs'imiz :DbContextten türetilirdi ama şuan DbContextten daha üst seviyede olan ve extra authentication özelliklerini
+    //bünyesinde barındıran tabloları olan IdentityDbContextten türettikc
+	public class IdentityContext: IdentityDbContext<IdentityUser>
+	{
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+                
+        }
+    }
+}
